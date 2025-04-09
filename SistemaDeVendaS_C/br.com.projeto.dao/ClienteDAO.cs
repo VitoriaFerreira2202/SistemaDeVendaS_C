a08b6c1c8f8522b4c6c877adbae5febee9b1fb5c
Vitoria Ferreira <vitoria.ferrsenac@gmail.com>
2025-04-09 17:25:45 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeVendaS_C/br.com.projeto.dao/ClienteDAO.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+using SistemaDeVendaS_C.br.com.projeto.conguicao;
+
+namespace SistemaDeVendaS_C.br.com.projeto.dao
+{
+    public class ClienteDAO
+    {
+        private MySqlConnection conexao;
+
+        public ClienteDAO()
+        {
+            this.conexao = new ConnectionFactory().getconnection();
+        }
+    }
+}
